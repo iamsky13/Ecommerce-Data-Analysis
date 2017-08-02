8b564d9a395de1887edb6d00ea966836f4004d84
Aakash <uniqueaakash13@hotmail.com>
2017-08-02 22:12:24 +0545

    some plot

--- Ecommerce data analysis/script.R
+++ Ecommerce data analysis/script.R
@@ -1,14 +1,18 @@
 #load train data
 train <- read.csv("mydata.csv", header = TRUE)
 
+#structure
 str(train)
 
+#changing datatype
 train$Sell = as.factor(train$Sell)
 
+#check
 table(train$Sell)
 
 table(train$city)
 
+#Converting city names just to make it easy 
 ecity <- function(name) {
     name <- as.character(name)
     if (length(grep("Hels", name)) > 0) {
@@ -31,3 +35,36 @@ for (i in 1:nrow(train)) {
 }
 
 train$city <- as.factor(city)
+
+table(train$city)
+
+library(ggplot2)
+
+ggplot(train, aes(x = city, fill = factor(Sell))) +
+geom_bar(width = 0.5) +
+xlab("City") +
+ylab("Number") +
+labs(fill = "Sells")
+
+
+ggplot(train, aes(x = Sex, fill = factor(Sell))) +
+geom_bar(width = 0.5) +
+facet_wrap(~city) +
+xlab("Gender") +
+ylab("Number") +
+labs(fill = "Sells")
+
+ggplot(train, aes(x = Sib_Sp, fill = factor(Sell))) +
+geom_bar(width = 0.5) +
+facet_wrap(~city) +
+xlab("sibling spouse") +
+ylab("Number") +
+labs(fill = "Sells")
+
+ggplot(train, aes(x = Par_ch, fill = factor(Sell))) +
+geom_bar(width = 0.5) +
+facet_wrap(~city) +
+xlab("parent children") +
+ylab("Number") +
+labs(fill = "Sells")
+
